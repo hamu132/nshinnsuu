b32b33a1907e5572a398028b5bb28b1fe925fd15
gurahamu <guba06120@gmail.com>
2025-05-20 23:53:33 +0900

    m

--- decode.py
+++ decode.py
@@ -1,23 +1,51 @@
 import nshinsuu
 
+
+#2進数に変換して、全部くっつける
+def nishinnsuukuttuke(contents,q):
+    print(contents)
+    byteList = []
+    for c in contents:
+        two = nshinsuu.changeN(c,10,2)
+        byteList.append(two)
+    print("".join(byteList))
+
+
+
+#10進数に変換して、そのままUTF-8,Unicodeを試す
 def decode(contents,codeType):
     byteList = []
     for c in contents:
-        ten = nshinsuu.toTen(c,int(codeType))
-        byteList.append(ten)
-    byte = bytes(byteList)
-    print(f"{byteList} -> {byte}")
-    utf8Decode(byte)
-    uniCodeDecode(byte)
+        ten = nshinsuu.changeN(c,int(codeType),10)
+        byteList.append(int(ten))
+    try:
+        byte = bytes(byteList)
+        print(f"10進数：{byteList} -> バイト：{byte}")
+        utf8Decode(byte)
+    except ValueError:
+        print(f"10進数：{byteList}")
+        pass
+    uniCodeDecode(byteList)
+
+
+
 
 def utf8Decode(byte):
+    print("utf-8の解読を試みます...")
     try:
-        print(byte.decode("utf-8"))
+        print(f"utf-8: {byte.decode("utf-8")} end")
     except UnicodeDecodeError:
         print("UTF-8は復号できません。")
+    print()
 
-def uniCodeDecode(byte):
-    try:
-        print(chr(byte))
-    except UnicodeDecodeError:
-        print("Unicodeは復号できません。")
+def uniCodeDecode(byteList):
+    print("Unicodeの解読を試みます...")
+    for b in byteList:
+        print(hex(b),end = " ")
+    print()
+    for b in byteList:
+        try:
+            print(chr(b),end = ", ")
+        except UnicodeDecodeError:
+            print("Null",end=", ")
+    print()
--- nshinsuu.py
+++ nshinsuu.py
@@ -1,26 +1,59 @@
 def changeN(data,fromN,toN):
-    return
+    if not str(data).isnumeric or not str(fromN).isnumeric or not str(toN).isnumeric:
+        return 1
+    if int(fromN)<2 or int(toN)<2:
+        return 1
+    
+    temp = toTen(data,fromN)
+    result = fromTen(temp,toN)
+    return result
 
 def fromTen(data,toN):
+    numToStr = {
+        0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"
+    }
     data = int(data)
     result = []
     temp = data
     while True:
         div = temp//toN
         mod = temp%toN
-        result.append(mod)
+        result = [numToStr[mod]] + result
         temp = div
         if temp==0:
             break
-    return "".join()
+    return "".join(result)
 
 def toTen(data,fromN):
-    dataList = [int(x) for x in list(data)]
+    strToNum = {
+        "0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"A":10,"B":11,"C":12,"D":13,"E":14,"F":15
+    }
+    dataList = [strToNum[x] for x in list(data)]
     if max(dataList)>=fromN:
+        print("想定しない数が入っているエラー")
         return 0
     result = 0
     for i in range(len(dataList)):
         index = len(dataList)-i-1
         result += dataList[index] * fromN ** i
-    return result
+    return str(result)
 
+
+def utf8Decode():
+    
+    print("2進数を入力してください（スペース,改行区切り・qで終了）")
+    byte = []
+    while True:
+        data = input().split(" ")
+        if data == ["q"]:
+            break
+        for i in data:
+            for w in list(i):
+                if w!="0" and w!="1":
+                    print(f"{w}:エラー\n2進数を入力してください")
+                    continue
+        
+        for d in data:
+            ten = int(toTen(d,2))
+            byte.append(ten)
+    return bytes(byte).decode("utf-8")
