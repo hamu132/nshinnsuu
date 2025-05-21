9928372628f37d481253ff3c5ef2e9338c6b8a50
gurahamu <guba06120@gmail.com>
2025-05-21 23:52:37 +0900

    a

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def start():
             print("パスが不正です。")
     contents = fileIO.openFile(path)
     print(contents,len(contents))
-    #とりあえずデータは2進数にするaasssssa
+    #とりあえずデータは2進数にするaasssssaa
     content = ""
     for c in contents:
         content+=nshinsuu.changeN(c,n,2)
